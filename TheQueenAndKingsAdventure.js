542d554a770587d8e7f14fcec7bba4117bd6a3cf
JwBZx <Jwbmw3@gmail.com>
2015-10-24 17:03:47 -0400

    TheQueenAndKingsAdventure File

---
+++ TheQueenAndKingsAdventure.js
@@ -0,0 +1,14 @@
+console.log("Welcome to The Queen and Kings Adventure, by Soderminal!");
+prompt("Do you want to play this fun game?");
+if(prompt === "yes") {
+	console.log("Sweet! Alright let's start the adventure!");
+} else {
+	console.log("But why not? :( You make me feel SADDU....");
+}
+console.log("One day The Queen and King were having a picnic, but then an evil Clemson Tiger appeared out of nowhere!");
+prompt("Should you run? (yes/no)");
+if(prompt === "yes") {
+	console.log("You ran and got away from the bad, bad tiger!");
+} else {
+	console.log("You stayed, but luckily the Royal Army was nearby to stop the Tiger!");
+}
