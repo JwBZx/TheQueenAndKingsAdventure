515af72e78aaf96b0472242923d8f4f2492cd0bc
JwBZx <Jwbmw3@gmail.com>
2015-10-25 15:32:45 -0400

    TheQueenAndKingsAdventure File

---
+++ StoryUpdate1.js
@@ -0,0 +1,20 @@
+console.log("Welcome to TheQueenAndKingsAdventure!");
+console.log("By @JwBZx");
+console.log("This is a simple 'Choose Your Own Adventure' game all about The Queen (Laura) and The Loser King (me)!");
+console.log("You can check out this project on GitHub at github.com/JwBZx/TheQueenAndKingsAdventure");
+console.log("Thank You for checking out this game and this project/repository and God Bless!");
+
+console.log("Welcome to The Queen and Kings Adventure, by Soderminal!");
+prompt("Do you want to play this fun game?");
+if(prompt === "yes") {
+	console.log("Sweet! Alright let's start the adventure!");
+} else {
+	console.log("But why not? :( You make me feel SADDU....");
+}
+console.log("One day The Queen and King were having a picnic, but then an evil Clemson Tiger appeared out of nowhere!");
+prompt("Should you run? (yes/no)");
+if(prompt === "yes") {
+	console.log("You ran and got away from the bad, bad tiger!");
+} else {
+	console.log("You stayed, but luckily the Royal Army was nearby to stop the Tiger!");
+}
